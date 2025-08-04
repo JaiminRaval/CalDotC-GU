26bb81f1cd924e826fef90f3db4005601b2eec15
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-08-05 02:17:27 +0530

    added operations Func

--- caldotc-gu/ViewController.swift
+++ caldotc-gu/ViewController.swift
@@ -39,12 +39,143 @@ class ViewController: UIViewController {
 //        setupCalcUI()
     }
     
+ 
+    
+    
+    
+    func resetTextField() {
+        if let input = ioField.text {
+            if input == "0" {
+                ioField.text = ""
+            }
+        }
+    }
+    
+    
     
     @IBAction func numPressed(_ sender: UIButton) {
+        guard let input = sender.titleLabel?.text else { return  }
+        
+        switch input {
+            
+        case "1":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)1"
+
+            }
+            
+        case "2":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)2"
+            }
+        
+        case "3":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)3"
+            }
+        case "4":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)4"
+            }
+        case "5":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)5"
+            }
+        case "6":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)6"
+            }
+        case "7":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)7"
+            }
+        case "8":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)8"
+            }
+        case "9":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)9"
+            }
+        case "0":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)0"
+            }
+        case "00":
+            resetTextField()
+            if let temp = ioField.text {
+                ioField.text = "\(temp)00"
+            }
+        default:
+            ioField.text = "0"
+            
+        }
+    //  debug using this something like this:
+//        print("button pressed: \(sender.titleLabel?.text!)")
+        
+        
         
     }
     
+    
+    
+    
+    
     @IBAction func operationPressed(_ sender: UIButton) {
+        
+        let operation = sender.tag
+        
+        switch operation {
+        case 0:
+            print("+")
+            if let appendedStr = ioField.text {
+                
+            }
+        case 1:
+            print("-")
+            if let appendedStr = ioField.text {
+
+            }
+        case 2:
+            print("/")
+            if let appendedStr = ioField.text {
+
+            }
+        case 3:
+            print("x")
+            if let appendedStr = ioField.text {
+
+            }
+        case 4:
+            print("=")
+            if let appendedStr = ioField.text {
+                var partsArr = appendedStr.split(separator: "+")
+                print("seperated vals = \(partsArr)")
+            }
+        case 5:
+            print("C")
+            if let appendedStr = ioField.text {
+
+            }
+        case 6:
+            print("delete one char")
+            if let appendedStr = ioField.text {
+
+            }
+           
+        default:
+            break
+        }
         
     }
 
