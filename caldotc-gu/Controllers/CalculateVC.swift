ae7f9432d2aeebae9602cac7fa35e3c1b99458df
JaiminRaval <siliconvalley.jaimin@gmail.com>
2026-02-24 14:18:30 +0530

    added title code

--- caldotc-gu/Controllers/CalculateVC.swift
+++ caldotc-gu/Controllers/CalculateVC.swift
@@ -13,19 +13,26 @@ class CalculateVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sended: \(btnPressedID)")
-        // Do any additional setup after loading the view.
+        if !btnPressedID.isEmpty {
+            print("sended: \(btnPressedID)")
+            setDynamicTitle()
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setDynamicTitle() {
+        switch btnPressedID {
+        case "+":
+            navigationItem.title = "Addition"
+        case "-":
+            navigationItem.title = "Substraction"
+        case "x":
+            navigationItem.title = "Multiplication"
+        case "/":
+            navigationItem.title = "Division"
+        default:
+            navigationItem.title = "nothing"
+        }
     }
-    */
 
 }
--- caldotc-gu/Controllers/HomeVC.swift
+++ caldotc-gu/Controllers/HomeVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
     var senderValue = ""
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,47 +16,42 @@ class HomeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var calcVC = segue.destination as? CalculateVC
         calcVC?.btnPressedID = senderValue
-
+        
     }
     
-    func goToSegue(){
+    func callPerformSegue(){
         performSegue(withIdentifier: "GoToCalculateVC", sender: self)
     }
-
+    
     //MARK: - add
     @IBAction func addBtnTapped(_ sender: UIButton) {
         senderValue = sender.titleLabel?.text ?? ""
-
-        goToSegue()
+        callPerformSegue()
         print(sender.titleLabel?.text ?? "")
     }
     @IBAction func subBtnTapped(_ sender: UIButton) {
         senderValue = sender.titleLabel?.text ?? ""
-
-        goToSegue()
-
+        callPerformSegue()
         print(sender.titleLabel?.text ?? "")
-
+        
     }
     @IBAction func multiBtnTapped(_ sender: UIButton) {
         senderValue = sender.titleLabel?.text ?? ""
-
-        goToSegue()
+        callPerformSegue()
         print(sender.titleLabel?.text ?? "")
-
+        
     }
     @IBAction func divBtnTapped(_ sender: UIButton) {
         senderValue = sender.titleLabel?.text ?? ""
-
-        goToSegue()
+        callPerformSegue()
         print(sender.titleLabel?.text ?? "")
-
+        
     }
-
+    
 }
 
